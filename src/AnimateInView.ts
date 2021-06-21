e27b0dba196158d46697a9e043206fdb9b0b34e7
Neerav Pandya <me@neerav.dev>
2021-06-21 11:24:07 +0530

    fixup! Added module declaration for use with TypeScript

--- src/AnimateInView.ts
+++ src/AnimateInView.ts
@@ -5,7 +5,7 @@ import useAnimateInView from '@/composables/useAnimateInView'
 
 const animateInView: Directive = {
     mounted(el:HTMLElement, binding:DirectiveBinding) {
-        const animation =   <AnimationParams>binding.value
+        const animation =   <string|AnimationParams>binding.value
         const repeat = <boolean> binding.modifiers.repeat || false
 
         useAnimateInView.apply(el, animation, repeat)
