cefe4987a81047130806c80579672434e504024c
Neerav Pandya <me@neerav.dev>
2021-06-25 09:57:40 +0530

    Removed build and composables redundant directories

--- composables/useScrollObserver.d.ts
+++
@@ -1,8 +0,0 @@
-import { scrollDetectionCallback } from "@/types/index";
-declare function detectScrollDirection(scrollDetectionHandler: scrollDetectionCallback): void;
-declare function removeEventListener(): void;
-declare const _default: {
-    detectScrollDirection: typeof detectScrollDirection;
-    removeEventListener: typeof removeEventListener;
-};
-export default _default;
